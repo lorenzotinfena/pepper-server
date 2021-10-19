5fee2cafad7cc9b76ecc0339145551ac9cd69eb9
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2021-10-19 11:56:21 +0200

    o

--- go.mod
+++ go.mod
@@ -4,14 +4,19 @@ go 1.17
 
 require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.6.0
+	github.com/kellydunn/golang-geo v0.7.0
 	google.golang.org/grpc v1.41.0
 	google.golang.org/protobuf v1.27.1
 )
 
 require (
+	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20211016002631-37fc39342514 // indirect
+	github.com/kylelemons/go-gypsy v1.0.0 // indirect
+	github.com/lib/pq v1.10.3 // indirect
+	github.com/ziutek/mymysql v1.5.4 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/genproto v0.0.0-20210903162649-d08c68adba83 // indirect
 )
--- server.go
+++ server.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"github.com/lorenzotinfena/chat-and-meet/proto" // Update
+	"github.com/kellydunn/golang-geo"
 )
 
 type server struct {
 
 }
-func (*server) Match(*proto.MatchRequest, proto.Service_MatchServer) error {
+func (*server) Match(matchRequest *proto.MatchRequest,serviceMatchServer proto.Service_MatchServer) error {
+	//my_gender := matchRequest.MyInfo.Gender
+	my_age := matchRequest.MyInfo.Age
+	my_location := geo.NewPoint(matchRequest.MyInfo.Latitude, matchRequest.MyInfo.Longitude)
+
+	//target_gender := matchRequest.Preferences.Gender
+	min_age := matchRequest.Preferences.MinAge
+	max_age := matchRequest.Preferences.MaxAge
+	//max_distance_km := matchRequest.Preferences.KilometersRange
+
+	// check parameters
+	if my_age < 18 || my_age > 100 || my_location.Lat() < -90 || my_location.Lat() > 90 || my_location.Lng() < -180 || my_location.Lat() > 180 || min_age < 18 || max_age > 100 || min_age > max_age{
+		return nil
+	}
 	return nil
+	
 }
 func (*server) StartChat(proto.Service_StartChatServer) error {
 	return nil
